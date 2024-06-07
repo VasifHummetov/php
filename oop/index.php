f87720991328e17ae4ca42e3cccb7807c4faf1ff
Vasif Hummetov <tapmaca777@gmail.com>
2024-06-07 20:41:53 +0400

    oop

--- oop/index.php
+++ oop/index.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($controller) {
-    require_once $controller.".php";
+    require_once __DIR__. '/' . str_replace('\\','/', $controller) .'.php';
 });
 
 Router::get('/', [\Http\Controllers\HomeController::class, 'index']);
@@ -9,5 +9,10 @@ Router::get('/about', [\Http\Controllers\HomeController::class, 'about']);
 Router::get('/products', [\Http\Controllers\HomeController::class, 'products']);
 Router::get('/product/{id}/{slug}', [\Http\Controllers\HomeController::class, 'product']);
 Router::get('/services', [\Http\Controllers\HomeController::class, 'services']);
+Router::get('/test/{name}', function ($name) {
+    return [
+
+    ];
+});
 
 Router::routes();
