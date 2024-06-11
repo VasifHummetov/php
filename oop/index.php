cd6d96679eaae40bbc2b3922ecf77459bc09cd49
Vasif Hummetov <vasif.hummetov.89@mail.ru>
2024-06-11 17:18:14 +0400

    Save changes to OOP/index.php

--- oop/index.php
+++ oop/index.php
@@ -9,8 +9,6 @@ Router::get('/about', [\Http\Controllers\HomeController::class, 'about']);
 Router::get('/products', [\Http\Controllers\HomeController::class, 'products']);
 Router::get('/product/{id}/{slug}', [\Http\Controllers\HomeController::class, 'product']);
 Router::get('/services', [\Http\Controllers\HomeController::class, 'services']);
-
-
 Router::get('/test/{name}', function ($name) {
     return [
 
