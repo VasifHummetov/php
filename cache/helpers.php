fee3737fe6cbe1949ae421321f30049f3da05ae3
Vasif Hummetov <vasif.hummetov.89@mail.ru>
2024-06-11 10:42:37 +0400

    cache

---
+++ cache/helpers.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @param string $key
+ * @param int|float $ttl zaman araligi
+ * @param callable $callback function
+ */
+function cache(string $key, int|float $ttl, callable $callback)
+{
+    $file = storage_path('cache' . DIRECTORY_SEPARATOR . $key . '.php');
+
+    if (file_exists($file)) {
+        $data = unserialize(file_get_contents($file));
+
+        if ($data['ttl'] > time()) {
+            return $data['data'];
+        } else {
+            unlink($file);
+        }
+    }
+
+    $data = [
+        'ttl' => time() + $ttl,
+        'data' => $callback()
+    ];
+
+    $data = serialize($data);
+
+    if (!file_exists($file)) {
+        file_put_contents($file, $data);
+    }
+
+    return unserialize($data)['data'];
+}
+
+/**
+ * @param string $file
+ * @return string
+ */
+function storage_path(string $file): string
+{
+    return __DIR__ . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . $file;
+}
+
+/**
+ * @param string $key
+ * @return void
+ */
+function cache_clear(string $key): void
+{
+    $file = storage_path('cache' . DIRECTORY_SEPARATOR . $key . '.php');
+
+    if (file_exists($file)) {
+        unlink($file);
+    }
+}
--- cache/index.php
+++ cache/index.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once 'helpers.php';
+
+
+// burda deyerleri deyisib yoxlaya bilersiz
+
+$products = cache('products', 10, function () {
+    return ['h'];
+});
+
+var_dump($products);
