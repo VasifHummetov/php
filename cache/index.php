215b914fe4315418f0cde7c16724c652e6b75a03
Vasif Hummetov <vasif.hummetov.89@mail.ru>
2024-06-11 11:11:23 +0400

    cache

--- cache/index.php
+++ cache/index.php
@@ -6,8 +6,8 @@ require_once 'helpers.php';
 // burda deyerleri deyisib yoxlaya bilersiz
 
 $products = cache('products', 10, function () {
-//    return file_get_contents('https://big.az');
-    return []; // mixed  value
+    return file_get_contents('https://big.az');
+    //return []; // mixed  value
 });
 
 echo $products;
