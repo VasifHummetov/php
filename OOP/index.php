4127cc80a6cf87775d6d99df775f3d944fae63e8
Vasif Hummetov <vasif.hummetov.89@mail.ru>
2024-06-11 17:20:59 +0400

    added index.php

---
+++ OOP/index.php
@@ -0,0 +1,18 @@
+<?php
+
+spl_autoload_register(function ($controller) {
+    require_once __DIR__. '/' . str_replace('\\','/', $controller) .'.php';
+});
+
+Router::get('/', [\Http\Controllers\HomeController::class, 'index']);
+Router::get('/about', [\Http\Controllers\HomeController::class, 'about']);
+Router::get('/products', [\Http\Controllers\HomeController::class, 'products']);
+Router::get('/product/{id}/{slug}', [\Http\Controllers\HomeController::class, 'product']);
+Router::get('/services', [\Http\Controllers\HomeController::class, 'services']);
+Router::get('/test/{name}', function ($name) {
+    return [
+
+    ];
+});
+
+Router::routes();
